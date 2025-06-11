c6648ef3599ad588b38c170ab8c1b8db662a1bec
Nikita Filin <nikita10filin@gmail.com>
2025-06-11 16:23:51 +0300

    api calls removed

--- src/components/Prices/PriceTable/PriceTable.jsx
+++ src/components/Prices/PriceTable/PriceTable.jsx
@@ -4,37 +4,11 @@ import useFetchPrices from "../../../hooks/useFetchPrices";
 
 function PriceTable() {
 
-    const {data} = useFetchPrices()
 
     return (
         <div className={Styles.pricesTableContainer} data-aos="zoom-in">
-            {data.map((category, index) => (
-                <div key={index} className={Styles.tableSection}>
-                    <h2>{category.category_name}</h2>
-                    <table className={Styles.table}>
-                        <thead>
-                        <tr>
-                            {category.headers.map(header => (
-                                <th key={header.id}>{header.name}</th>
-                            ))}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {Object.values(category.services).map((service, serviceIndex) => (
-                            <tr key={serviceIndex}>
-                                <td>{service.name}</td>
-                                {category.headers.slice(0, -1).map(header => (
-                                    <td key={header.id}>
-                                        {service.prices[header.id]}€
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                        </tbody>
 
-                    </table>
-                </div>
-            ))}
+
         </div>
     )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import MainLayout from "@/layouts/MainLayout";
 import state from "@/state";
-import { useAuth } from "@/hooks/useAuth";
 import "@/global.css";
 import {appWithTranslation, useTranslation} from 'next-i18next'
 
